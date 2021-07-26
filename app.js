55503416abccae171b8e9b2a0704fe480d48592d
Cecil <cvogrinetz@gmail.com>
2021-07-26 16:14:31 -0700

    Finished basics of TS

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-var add = function (num1, num2) {
-    return num1 + num2;
+var add = function (num1, num2, showResult, phrase) {
+    var result = num1 + num2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return result;
+    }
 };
 var n1 = 5;
 var n2 = 2.5;
-var result = add(n1, n2);
-console.log(result);
+var printResult = true;
+var resultPhrase = "Result is:  ";
+add(n1, n2, printResult, resultPhrase);
