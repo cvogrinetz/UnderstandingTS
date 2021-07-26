8e9b8a448e05c43877449d83e4af9439b009bdb0
Cecil <cvogrinetz@gmail.com>
2021-07-26 15:38:15 -0700

    Learning basics about core types

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("Your code goes here...");
+var add = function (num1, num2) {
+    return num1 + num2;
+};
+var n1 = 5;
+var n2 = 2.5;
+var result = add(n1, n2);
+console.log(result);
--- app.ts
+++ app.ts
@@ -1,1 +1,10 @@
-console.log("Your code goes here...");
+const add = (num1: number, num2: number) => {
+  return num1 + num2
+}
+
+const n1 = 5
+const n2 = 2.5
+
+const result = add(n1, n2)
+console.log(result)
+
