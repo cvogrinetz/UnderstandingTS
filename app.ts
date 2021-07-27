358b2f3c157d8054444c21fa54d45129f77442d2
Cecil <cvogrinetz@gmail.com>
2021-07-27 12:52:00 -0700

    Add enum examples

--- app.ts
+++ app.ts
@@ -1,8 +1,8 @@
 const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
+  name: string; // string
+  age: number; // number
+  hobbies: string[]; // an array of strings
+  role: [number, string]; // Tuple. (fixed length array)
 } = {
   name: "Bitterfunk",
   age: 99,
@@ -20,3 +20,32 @@ console.log(person.name);
 for (const hobby of person.hobbies) {
   console.log(hobby);
 }
+
+// ENUM EXAMPLE
+
+// Instead of declaring multiple variables like so...
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+// Enum can be used to create a custom type. enum will assign these varialbes with somewhat of an ID
+// enum Role {
+//   ADMIN,   ===> 0
+//   READ_ONLY, ====> 1
+//   AUTHOR,  =====> 2
+// }
+
+// YOU CAN ALSO DECLARE THE ID TO WHATEVER YOU NEED
+// EXAMPLE
+// enum Role {
+//   ADMIN = 5,   =====> ANY NUMBER
+//   READ_ONLY = 'string', ====> ANY STRING
+//   AUTHOR,  =====> 6  IF NOT DECLARED IT WILL ITERATE TO WHATEVER NUMBER THAT IS DEVALRED BEFOREs
+// }
+
+// const person = {
+//   name: "Bitterfunk",
+//   age: 99,
+//   hobbies: ["Sports", "Cooking"],
+//   role: ,
+// };
